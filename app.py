f698320b05441bb097575886522183b7ecfe525f
Mateus-Bauerr <127158016+Mateus-Bauerr@users.noreply.github.com>
2023-06-26 19:59:00 -0300

    teste com a API

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template ("index.html")
+# Dicionário para armazenar as traduções
+translations = {
+    "hello": {
+        "en": "Hello",
+        "es": "Hola",
+        "fr": "Bonjour"
+    },
+    "dog": {
+        "en": "Dog",
+        "es": "Perro",
+        "fr": "Chien"
+    }
+}
+
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    data = request.get_json()
+    word = data.get("word")
+    language = data.get("language")
+
+    if word in translations and language in translations[word]:
+        translated_word = translations[word][language]
+        return jsonify({"translation": translated_word})
+    else:
+        return jsonify({"error": "Tradução não encontrada."})
+
+
+if __name__ == "__main__":
+    app.run()
